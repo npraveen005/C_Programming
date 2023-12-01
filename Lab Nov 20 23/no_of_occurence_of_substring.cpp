7077f525439052fcf4bf7710b882861b082e365d
npraveen005 <143787529+npraveen005@users.noreply.github.com>
2023-12-01 14:16:06 +0530

    Update no_of_occurence_of_substring.cpp

--- Lab Nov 20 23/no_of_occurence_of_substring.cpp
+++ Lab Nov 20 23/no_of_occurence_of_substring.cpp
@@ -5,21 +5,28 @@
 #include <stdlib.h>
 
 int main(){
+
+	int i,j,c = 0,count = 0,n,l,size;
+	printf("Enter maximum string size:- ");
+	scanf("%d",&n);
 	char a[20], b[20];
-	int i,j,c = 0,count = 0,n,l;
+	
+	printf("Enter the string:- ");
+	fflush(stdin);
+	scanf("%[^\n]s",a);
+	fflush(stdin);
+	printf("Enter the substring to be counted:- ");
+	scanf("%[^\n]s",b);
+	scanf("%*c");
+	
 	n = strlen(a);
 	l = strlen(b);
 	
-	fflush(stdin);
-	scanf("%[^\n]",a);
-	fflush(stdin);
-	scanf("%[^\n]",b);
-	
 	for (i=0;i<n-l+1;i++){
 		for (j=0;j<l;j++){
+			
 			if(b[j] == a[i+j]) {
 				c++;
-				printf("%d",c);
 			}
 		}
 		if (c == l){
@@ -28,6 +35,6 @@ int main(){
 		c=0;
 	}
 	
-	printf("%d",count);
+	printf("Count of '%s' in '%s' is:- %d",b,a,count);
 	
 }
